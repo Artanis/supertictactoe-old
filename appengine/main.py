f61b78e1a9d1f70286c8402d43d666978dfca94f
Erik Youngren <artanis.00@gmail.com>
2011-01-29 16:20:41 -0800

    Added new game handler

--- appengine/main.py
+++ appengine/main.py
@@ -1,6 +1,7 @@
 # System Modules
 import sys
 import pickle
+import uuid
 
 # External Modules
 from django.utils import simplejson as json
@@ -12,19 +13,36 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 import web
 
 # Local Modules
-#import supertictactoe
+from supertictactoe import SuperTicTacToe
 
 # Application
 render = web.template.render('templates', base='__base__')
 
 class Game(db.Model):
-    player_x = db.UserProperty(required=true)
+    player_x = db.UserProperty(required=True)
     player_o = db.UserProperty()
-    game_state = False
+    move_x = db.BooleanProperty(default=True)
+    _game_state = db.BlobProperty()
+    
+    def __fget_game_state(self):
+        return SuperTicTacToe.load(self._game_state)
+    
+    def __fset_game_state(self, value):
+        self._game_state = value.dump()
+    
+    game_state = property(__fget_game_state, __fset_game_state)
 
 class MainPageHandler(webapp.RequestHandler):
     def get(self):
-        game_id = self.request.get('game_id')
+        """Entry point for application.
+        
+        Logs the user in (if he hasn't yet) and then redirects to the
+        web client, which handles the rest over Ajax.
+        
+        There will be another handler to do non-redirect logins.
+        Eventually.
+        
+        """
         user = users.get_current_user()
         
         if user is None:
@@ -42,7 +60,28 @@ class MoveHandler(webapp.RequestHandler):
         board = self.request.get('board')
         square = self.request.get('square')
         
+        game = Game.get_by_key_name(game_id)
+        
         print >>sys.stderr, game_id, (board, square)
+
+class GameFactory(webapp.RequestHandler):
+    def post(self):
+        user = users.get_current_user()
+        
+        if user is not None:
+            game_id = str(uuid.uuid1())
+            
+            game = Game(key_name = game_id, player_x = user)
+            game.game_state = SuperTicTacToe()
+            game.put()
+            
+            channel.send_message(user.user_id(), json.dumps({
+                'mode': "new-game",
+                'game_id': game_id}))
+        else:
+            # not going to catch not-logged-in everywhere. User should
+            # login before this stage.
+            self.error(401)
 
 class ChannelFactory(webapp.RequestHandler):
     """Serves channels by Ajax"""
@@ -65,7 +104,7 @@ class ChannelFactory(webapp.RequestHandler):
         """Echo back testing"""
         
         channel_id = self.request.get('channel_id')
-        print >>sys.stderr, "(",channel_id, ")"
+        
         if channel_id:
             channel.send_message(channel_id, json.dumps({
             'mode': 'channel-test',
@@ -75,6 +114,7 @@ def main():
     app = webapp.WSGIApplication([
         ('/', MainPageHandler),
         ('/channel', ChannelFactory),
+        ('/new-game', GameFactory),
         ('/move', MoveHandler)])
     
     run_wsgi_app(app)
